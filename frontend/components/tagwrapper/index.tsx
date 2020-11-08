705ea0f0561443a2c35265932e7d12a5bc35f8d7
Jeffrey Emakpor <jeffreyefemena4@gmail.com>
2020-11-08 09:27:00 +0000

    cleanup

---
+++ frontend/components/tagwrapper/index.tsx
@@ -0,0 +1,55 @@
+import { useRouter } from 'next/dist/client/router';
+import React from 'react';
+
+import { useEventContext } from '../context/Event';
+import Tags from '../tags/tags';
+import { Box } from '@chakra-ui/core';
+
+const TagName = [
+  {
+    id: 0,
+    label: 'All',
+    event: null,
+  },
+  {
+    id: 1,
+    label: 'Role Set',
+    event: 'roleSet',
+  },
+  {
+    id: 2,
+    label: 'Initialised',
+    event: 'initialised',
+  },
+  {
+    id: 3,
+    label: 'Payout Claimed',
+    event: 'payments',
+  },
+  {
+    id: 4,
+    label: 'Domain Added',
+    event: 'domainAdded',
+  },
+];
+
+const TagWrapper = () => {
+  const { replace } = useRouter();
+  return (
+    <Box className='container-tagWrapper' d='flex' width='100%' justifyContent='center'>
+      {TagName.map(({ id, label, event }) => {
+        return (
+          <Tags
+            key={id}
+            tagName={label}
+            onClick={() => {
+              event ? replace(`/${event}`) : replace(`/`);
+            }}
+          />
+        );
+      })}
+    </Box>
+  );
+};
+
+export default TagWrapper;
